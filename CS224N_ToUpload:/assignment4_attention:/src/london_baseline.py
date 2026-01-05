a6528a0523a04fe56aff20518fa376fab0317817
yahaha0123 <x235259@163.com>
2026-01-05 14:52:03 +0800

    Add assignment 1 & 4 and README

---
+++ CS224N_ToUpload:/assignment4_attention:/src/london_baseline.py
@@ -0,0 +1,28 @@
+# TODO: [part d]
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+
+import argparse
+import utils
+
+def main():
+    accuracy = 0.0
+
+    # Compute accuracy in the range [0.0, 100.0]
+    ### YOUR CODE HERE ###
+    dev_file = "birth_dev.tsv"
+    with open(dev_file) as f:
+        predictiuons = ["London"] * len(f.readlines())
+    total,correct = utils.evaluate_places(dev_file,predictiuons)
+    accuracy = correct/total
+    pass
+    ### END YOUR CODE ###
+
+    return accuracy
+
+if __name__ == '__main__':
+    accuracy = main()
+    with open("london_baseline_accuracy.txt", "w", encoding="utf-8") as f:
+        f.write(f"{accuracy}\n")
